c3a4a39ae73ed466f34a3b00683405473b8291e0
wvengen <dev-rails@willem.engen.nl>
2014-07-03 17:42:39 +0200

    class variable for use in layout

--- app/controllers/better_exception_app/http_errors_controller.rb
+++ app/controllers/better_exception_app/http_errors_controller.rb
@@ -11,30 +11,30 @@ class BetterExceptionApp::HttpErrorsController < ApplicationController
   def show
     # Pull from path info instead of params since we will get
     # params of real request instead of params for this app.
-    error = BetterExceptionApp::HttpError.new \
+    @error = BetterExceptionApp::HttpError.new \
       request.env["PATH_INFO"][1..-1],
       request.env['action_dispatch.exception']
 
-    if error.formattable? request.format
+    if @error.formattable? request.format
       # If they requested a format other than HTML just give them the
       # raw data in their format like the default exception app.
-      render :status => error.status,
-        request.format.to_sym => error.formatted(request.format)
+      render :status => @error.status,
+        request.format.to_sym => @error.formatted(request.format)
     else
       # If they are cool with HTML (or we don't know how to convert
       # to the format they requested) then kick in our magic.
-      if error.template
+      if @error.template
         # First try static template. Mainly for 500 Internal Server Error
         # since we cannot be sure they can render in a layout.
-        render error.template, :layout => false, :status => error.status
-      elsif error.description
+        render @error.template, :layout => false, :status => @error.status
+      elsif @error.description
         # If static error doesn't exist try a i18n-based description
         # within the layout.
-        render :text => error.description, :layout => true, :status => error.status
+        render :text => @error.description, :layout => true, :status => @error.status
       else
         # If not a static file or boilerplate description doesn't exist
         # then just get the exception to do the talking.
-        render :text => error.exception.message, :layout => true, :status => error.status
+        render :text => @error.exception.message, :layout => true, :status => @error.status
       end
     end
   end
